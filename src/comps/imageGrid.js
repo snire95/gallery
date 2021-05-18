64bfd8196264af22cf24233774ce1620f6c42e0c
snire95 <snire995@gmail.com>
2021-05-18 12:05:49 +0300

    two commit

--- src/comps/imageGrid.js
+++ src/comps/imageGrid.js
@@ -7,10 +7,20 @@ const ImageGrid = ({setSelectedImg}) => {
  return (
    <div className="img-grid">
     {docs && docs.map(doc => (
+      <div>
      <motion.div className = "img-wrap" key={doc.id} layout whileHover = {{opacity : 1}} onClick={() => setSelectedImg(doc.url)}>
       <motion.img src = {doc.url} alt = "uploaded pic" initial = {{opacity: 0}} animate = {{opacity : 1}} transition = {{delay : 1}}  />
+      
+   
      </motion.div>
+     <a href={doc.url} download="filename">
+    <button class="btn"><i class="fa fa-download"></i> Download</button></a>
+
+  {/* <a href={doc.url} target="_blank">Download</a> */}
+
+</div>
     ))}
+
    </div>
   )
 }
